9e85374b08ba0af284aa4bdc7f6b1c5ca8cab643
Frederic Marx <marx.frederic@gmail.com>
2015-05-18 22:23:01 +0200

    Add content

--- app/_content/slides.js
+++ app/_content/slides.js
@@ -3,19 +3,122 @@ const slides = [
 {
   id: 1,
   title: 'Ember.js: Die&nbsp;größten Stützräder der&nbsp;Welt',
-  main: '@fredericmarx'
-},
-{
-  id: 1243,
-  title: 'Trok',
-  main: 'Glowacki'
+  main: '<p>@<a href="https://twitter.com/fredericmarx" rel="me">fredericmarx</a></p>'
 },
 {
   id: 2,
-  title: 'Slide 2',
-  main: 'Test'
+  title: '“A framework for ambitious web applications”'
+},
+{
+  id: 3,
+  title: 'Was macht eine Website zur Web Application?'
+},
+{
+  id: 4,
+  title: 'UX'
+},
+{
+  id: 4.1,
+  title: 'UX',
+  main:`
+  <ul>
+    <li>Input / Output</li>
+  </ul>
+  `
+},
+{
+  id: 4.2,
+  title: 'UX',
+  main:`
+  <ul>
+    <li>Input / Output</li>
+    <li>Asynchron</li>
+  </ul>
+  `
+},
+{
+  id: 4.3,
+  title: 'UX',
+  main:`
+  <ul>
+    <li>Input / Output</li>
+    <li>Asynchron</li>
+    <li>Native™ User Experience</li>
+  </ul>
+  `
+},
+{
+  id: 5,
+  title: 'Infrastruktur'
+},
+{
+  id: 5.1,
+  title: 'Infrastruktur',
+  main:`
+  <ul>
+    <li>Routing</li>
+  </ul>
+  `
+},
+{
+  id: 5.2,
+  title: 'Infrastruktur',
+  main:`
+  <ul>
+    <li>Routing</li>
+    <li>Bound Templates</li>
+  </ul>
+  `
+},
+{
+  id: 5.3,
+  title: 'Infrastruktur',
+  main:`
+  <ul>
+    <li>Routing</li>
+    <li>Bound Templates</li>
+    <li>Data Models</li>
+  </ul>
+  `
+},
+{
+  id: 5.4,
+  title: 'Infrastruktur',
+  main:`
+  <ul>
+    <li>Routing</li>
+    <li>Bound Templates</li>
+    <li>Data Models</li>
+    <li>API</li>
+  </ul>
+  `
+},
+{
+  id: 'ayoade',
+  main:'<iframe src="//giphy.com/embed/L4AQvif7x9fS8" width="480" height="277" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+},
+{
+  id: 'dev',
+  main:`
+  <blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr">web developer (n)&#10;&#10;Someone who is paid to repeatedly reimplement a web browser in JavaScript to avoid page reloads.</p>&mdash; Ross Penman (@PenmanRoss) <a href="https://twitter.com/PenmanRoss/status/579778675865993216">March 22, 2015</a></blockquote>
+  `
+},
+{
+  id: 'rad',
+  title: 'Das Rad jedes Mal neu zu erfinden ist mühsam.',
+},
+{
+  id: 'repeat',
+  main:'<iframe src="//giphy.com/embed/jGa1qMM9EM944" width="480" height="314" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+},
+{
+  id: 'frameworks',
+  title: 'Deswegen gibt es Frameworks.',
+},
+{
+  id: 'magic',
+  main:'<iframe src="//giphy.com/embed/n0WvhHFTpihk4" width="480" height="274" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
 }
-
 ]
 
 export default slides;
--- app/models/slide.js
+++ app/models/slide.js
@@ -4,7 +4,6 @@ import fixtures from '../_content/slides';
 const slideModel = DS.Model.extend({
   title: DS.attr('string'),
   main: DS.attr('string'),
-  aside: DS.attr('string'),
   footer: DS.attr('string'),
   color: DS.attr('string'),
   backgroundColor: DS.attr('string'),
